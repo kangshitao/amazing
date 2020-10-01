5f0101e08f0c92f2ea9429620aae1c4ac48c5c60
Kangshitao <kang17853131742@gmail.com>
2020-10-01 20:55:48 +0800

    优化文件路径参数设置

--- wordcloud/main.py
+++ wordcloud/main.py
@@ -3,15 +3,22 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
 import jieba
-
+import argparse
 
 text_url = 'text/一路向北.txt'  # 文件路径
 fonts_path = '‪C:/Windows/Fonts/STKAITI.TTF'  # 字体文件
 img_path = 'figure/AE86.png'   # 图片路径
 
+ap = argparse.ArgumentParser()
+ap.add_argument('-text', default=text_url, help='text path')
+ap.add_argument('-font', default=fonts_path, help='font path')
+ap.add_argument('-mask', default=img_path, help='mask figure path')
+opt = vars(ap.parse_args())
+print(opt)
+
 
 def get_text():  # 获取文本内容
-    content = open(text_url, 'r', encoding='utf-8').read()
+    content = open(opt['text'], 'r', encoding='utf-8').read()
     # 中文分词
     result = jieba.cut(content)  # 精准模式分词
     text = ' '.join(result)
@@ -19,8 +26,8 @@ def get_text():  # 获取文本内容
 
 
 def GenerateWordCloud(text):    # 生成词云对象
-    mask_figure = np.array(Image.open(img_path))
-    wc = WordCloud(font_path= fonts_path,
+    mask_figure = np.array(Image.open(opt['mask']))
+    wc = WordCloud(font_path= opt['font'],
                    max_words=500,   # 显示最大词数
                    scale=10,   # 比例放大，数值越大词云越清晰
                    background_color='white',
